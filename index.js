f3f6c06e59669ba2507acad6ef921b606ea69196
Aayush Gupta <93078212+ayush827127@users.noreply.github.com>
2023-04-08 14:09:38 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function showAlert() {
-    // Get the alert element
-    var alertElement = document.getElementById("myAlert");
-    
-    // Show the alert
-    alertElement.classList.remove("d-none");
-  }
