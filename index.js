89e68b29fbd4ce4bc7f408de9df866151f1de1e9
Aayush Gupta <93078212+ayush827127@users.noreply.github.com>
2023-04-08 14:10:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    // Get the alert element
+    var alertElement = document.getElementById("myAlert");
+    
+    // Show the alert
+    alertElement.classList.remove("d-none");
+  }
